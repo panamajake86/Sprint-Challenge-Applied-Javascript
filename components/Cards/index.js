451d7591fe36677955eb65e301382f686185124e
Jake <panamajake86@gmail.com>
2019-09-13 21:29:37 -0600

    finished the MVP. Not sure I'm going to get to the stretch stuff.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,58 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(results => {
+        console.log(results);
+        const cardsCont = document.querySelector('.cards-container');
+        results.data.articles.bootstrap.forEach(e => {
+            cardsCont.appendChild(card(e));
+        });
+        results.data.articles.javascript.forEach(e => {
+            cardsCont.appendChild(card(e));
+        });
+        results.data.articles.technology.forEach(e => {
+            cardsCont.appendChild(card(e));
+        });
+        results.data.articles.jquery.forEach(e => {
+            cardsCont.appendChild(card(e));
+        });
+        results.data.articles.node.forEach(e => {
+            cardsCont.appendChild(card(e));
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+function card(whoopty) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = document.createElement('div');
+    headline.textContent = whoopty.headline;
+    headline.classList.add('headline');
+    card.appendChild(headline);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    card.appendChild(author);
+
+
+    const imgCont = document.createElement('div');
+    imgCont.classList.add('img-container');
+    author.appendChild(imgCont);
+
+    const image = document.createElement('img');
+    image.setAttribute('src', whoopty.authorPhoto);
+    image.classList.add('img');
+    imgCont.appendChild(image);
+
+    const span = document.createElement('span');
+    span.textContent = `By ${whoopty.authorName}`;
+    span.classList.add('span')
+    author.appendChild(span);
+
+    return card;
+};
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,23 +8,32 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-// axios.get('https://lambda-times-backend.herokuapp.com/topics')
-//     .then(results => {
-//         const topics = document.querySelector('.topics');
-//         topics.appendChild(tabs(results));
-//     });
-
-function tabs (schtuff) {
-    schtuff.data.forEach(e => {
-        const tab = document.createElement('div');
-        tab.textContent = e;
-        tab.classList.add('tab');
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(results => {
+        const top = document.querySelector('.topics');
+        results.data.topics.forEach(e => {
+            top.appendChild(tabs(e));
+        })
+    .catch(err=>{
+        console.log(err);
     });
-};
+    });
 
-//     topics: Array(5)
-// 0: "javascript"
-// 1: "bootstrap"
-// 2: "technology"
-// 3: "jquery"
-// 4: "node.js"
+    function tabs (schtuff) {
+        const tab = document.createElement('div');
+        tab.textContent = schtuff;
+        tab.classList.add('tab');
+        
+        return tab;
+    };
+
+// const testData = [
+//     "javascript",
+//     "bootstrap",
+//     "technology",
+//     "jquery",
+//     "node.js"
+//    ];
+
+
+
